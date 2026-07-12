5f39e1a46286d7d85bde65489cb7388224e29854
AlejandroGuillermo7 <ale1111prepa7@gmail.com>
2026-07-12 17:02:30 -0600

    Api

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_URL = "https://dummyjson.com/auth/login";
+
+export async function loginUser(username, password) {
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, password }),
+  });
+
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.message || "Usuario o contraseña incorrectos");
+  }
+
+  return data; 
+}
