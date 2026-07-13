777622ab639ff26ae6ef0c9a3b699fec449e1a0c
AlejandroGuillermo7 <ale1111prepa7@gmail.com>
2026-07-12 22:24:24 -0600

    Editar Producto 80%

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import "./App.css";
 import Navbar from "./components/Navbar.jsx";
 import Sidebar from "./components/Sidebar.jsx";
 import Login from "./components/Login.jsx"
+import EditarProductoModal from "./components/EditarProductoModal.jsx";
 
 function App() {
   const [sidebarAbierto, setSidebarAbierto] = useState(false);
   const [persona, setPersona ] = useState(null)
+  /*const [mostrarModal, setMostrarModal] = useState(false); */
 
 
   function abrirSidebar() {
@@ -28,6 +30,13 @@ function App() {
     setPersona(null);
   }
 
+
+  /* Es solo probar el from
+  function guardarProducto(datos) { 
+    console.log("Producto guardado:", datos);
+    setMostrarModal(false);
+  }
+  */
 
   if (!persona) {
     return <Login onLoginExitoso={alIniciarSesion} />;
@@ -53,8 +62,30 @@ function App() {
 
         <main className="contenidoPagina">
           <h1>Lista de productos</h1>
+
+          {/* BOTÓN TEMPORAL SOLO PARA PROBAR EL MODAL 
+          <button onClick={() => setMostrarModal(true)}>
+            Probar modal editar producto
+          </button>
+          /*Solo es prueba para verlo en pantalla */}
+
+
         </main>
       </div>
+
+
+      {/*Solo es prueba para verlo en pantalla */
+       /* el modal aparece encima de todo cuando mostrarModal es true 
+      {mostrarModal && (
+        <EditarProductoModal
+          producto={null}
+          onGuardar={guardarProducto}
+          onCancelar={() => setMostrarModal(false)}
+        />
+      )}
+     BORRAR
+     */}
+
     </div>
   );
 }
